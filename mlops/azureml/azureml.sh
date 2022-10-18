c714826c11fb0331a2f9b6b3ba144e9a887979ce
Maggie Mhanna <maggiemhanna@gmail.com>
2022-10-18 09:10:59 +0000

    remove standalone job

--- mlops/azureml/azureml.sh
+++ mlops/azureml/azureml.sh
@@ -1,22 +1,11 @@
 # TRAIN
 
 # Create train job compute cluster
-az ml compute create -n cpu-cluster --type amlcompute --min-instances 0 --max-instances 4
-
-# Create data asset
-az ml data create --file data/data.yml
-
-# Create train environment
-az ml environment create --file environment/train-env.yml
-
-# Create standalone job
-az ml job create --file train/train.yml
-
+az ml compute create --file train/compute.yml
+# Register train environment 
+az ml environment create --file train/environment.yml
 # Create pipeline job
 az ml job create --file train/pipeline.yml
-
-# Create pipeline rai job
-az ml job create --file train/pipeline-rai.yml
 
 # DEPLOY
 
@@ -25,7 +14,7 @@ az ml job create --file train/pipeline-rai.yml
 # create online endpoint
 az ml online-endpoint create --file deploy/online/online-endpoint.yml
 # create online deployment
-az ml online-deployment create --file deploy/online/online-deployment.yml
+az ml online-deployment create --file deploy/online/online-deployment.yml 
 # allocate traffic
 az ml online-endpoint update --name taxi-online-endpoint --traffic blue=100
 # invoke and test endpoint
