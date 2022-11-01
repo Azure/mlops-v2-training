c8f6568ce6dd7c31bbd76237a2a7c4ec9c642f17
Maggie Mhanna <maggiemhanna@gmail.com>
2022-11-01 00:21:37 +0000

    register train data as data asset and use as inputs for pipeline runs

--- mlops/azureml/azureml.sh
+++ mlops/azureml/azureml.sh
@@ -2,6 +2,8 @@
 
 # Create train job compute cluster
 az ml compute create --file train/compute.yml
+# Register data asset 
+az ml data create --file train/data.yml
 # Register train environment 
 az ml environment create --file train/environment.yml
 # Create pipeline job
